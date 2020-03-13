cee9b3e2a7f5db5323ff30da5b3c44d18e8a139d
jwchoi09 <jwchoi09@yonsei.ac.kr>
2020-03-14 03:55:53 +0900

    Update 2_RPi4_download_opencv.sh

--- script/2_RPi4_download_opencv.sh
+++ script/2_RPi4_download_opencv.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
+
+# Specifying the OpenCV version
+cv_v="master"
+
+cwd=$(pwd)/opencv
+mkdir -p $cwd
+cd $cwd
+
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkout $cvVersion
+cd ..
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv_contrib
+git checkout $cv_v
+cd ..
+cd opencv
+
+
+
+# Apply Patch
+# https://github.com/AastaNV/JEP/blob/master/script/install_opencv4.1.1_Jetson.sh
+sed -i 's/include <Eigen\/Core>/include <eigen3\/Eigen\/Core>/g' modules/core/include/opencv2/core/private.hpp
+
+
+
+
+
+
+
+
+
 
